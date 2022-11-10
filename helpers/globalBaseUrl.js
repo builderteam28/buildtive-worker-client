fa2e50d3141fe95299ebac6aa294dc8572ed30ff
Chossy Pratama <chossy.pratama@ti.ukdw.ac.id>
2022-11-11 00:24:42 +0700

    new globalURL

--- helpers/globalBaseUrl.js
+++ helpers/globalBaseUrl.js
@@ -1,4 +1,5 @@
 // const globalBaseURL = 'https://570a-125-167-238-45.ap.ngrok.io';
-const globalBaseURL = 'https://buildtive-server.herokuapp.com';
+// const globalBaseURL = 'https://buildtive-server.herokuapp.com';
+const globalBaseURL = 'https://build-hub-server.herokuapp.com';
 
 export default globalBaseURL;
